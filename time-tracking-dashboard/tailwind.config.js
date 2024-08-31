249b2507e086b66d149a9079d68f48f12457bfc1
Shruti Shinde <sss22@somaiya.edu>
2024-08-31 23:52:33 +0530

    time tracking dashboard initial commit

---
+++ time-tracking-dashboard/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "hsl(246, 80%, 60%)",
+        lightRed: "hsl(15, 100%, 70%)",
+        softBlue: "hsl(195, 74%, 62%)",
+        limeGreen: "hsl(145, 58%, 55%)",
+        studyLightRed: "hsl(348, 100%, 68%)",
+        violet: "hsl(264, 64%, 52%)",
+        softOrange: "hsl(43, 84%, 65%)",
+        veryDarkBlue: "hsl(226, 43%, 10%)",
+        darkBlue: "hsl(235, 46%, 20%)",
+        desaturedBlue: "hsl(235, 45%, 61%)",
+        paleBlue: "hsl(236, 100%, 87%)",
+      },
+      fontFamily: {
+        rubik: "Rubik",
+      },
+    },
+  },
+  plugins: [],
+};
